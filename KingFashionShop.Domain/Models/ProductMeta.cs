da92bf359edf3e721f01199f9a3c3b4b73857d6e
tran van duy <vanduy9491@gmail.com>
2021-09-17 20:49:19 +0700

    Add project files.

---
+++ KingFashionShop.Domain/Models/ProductMeta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KingFashionShop.Domain.Models
+{
+    public class ProductMeta
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string Key { get; set; }
+        public string Content { get; set; }
+        public Product Product { get; set; }
+    }
+}
